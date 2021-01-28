52bfc4f0be952fcb9f268533127e16460e975c8a
theerapat-pkcn <theerapat.pkcn@gmail.com>
2021-01-28 16:21:10 +0700

    second version

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
+var path = require("path")
 var express = require("express")
 var app = express();
+
+// Express middleware for serving static files
+//app.use(express.static(path.join(__dirname, "pkcn_web")));
+app.use(express.static(__dirname));
+
+//hello is another callback
+app.get('/', (req, res) => {
+    res.redirect("/index.html")
+})
+
+function hello(request, response) {
+    //response.send("Hello world test");
+    console.log(request.path);
+    response.sendFile('/index.html', {root: __dirname })
+}
 
 // 'listen' is a callback function
 var server = app.listen(process.env.PORT || 3000, listen);
@@ -8,12 +24,4 @@ function listen(){
     var host = server.address().address;
     var port = server.address().port;
     console.log("Example app is listening at http://" + host + ":" + port);
-}
-
-//hello is another callback
-app.get('/', hello)
-
-function hello(request, response) {
-    //response.send("Hello world test");
-    response.sendFile('/index.html', {root: __dirname })
 }
